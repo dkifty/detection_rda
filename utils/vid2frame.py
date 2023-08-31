402a7d4592cdb2aebda4d21371a50e9f47191e85
dkifty <143667937+dkifty@users.noreply.github.com>
2023-08-31 13:29:00 +0900

    Update vid2frame.py

--- utils/vid2frame.py
+++ utils/vid2frame.py
@@ -7,57 +7,35 @@ import numpy as np
 import os
 
 def vid2frame(vid_path, save_path, name, frame):
-    '''
-    Video to frames
-
-    Params: 동영상 경로, 프레임 저장 경로, 저장 프레임 이름
-    Return:
-    '''
-
-    vid = cv2.VideoCapture(vid_path)                # 동영상 로드
-    frame_num = vid.get(cv2.CAP_PROP_FRAME_COUNT)   # 전체 프레임 수
+    vid = cv2.VideoCapture(vid_path) 
+    frame_num = vid.get(cv2.CAP_PROP_FRAME_COUNT)
     print('FRAME NUM: %d' % frame_num)
-    fps = vid.get(cv2.CAP_PROP_FPS)                 # 현재 FPS
+    fps = vid.get(cv2.CAP_PROP_FPS)
     print('FPS: %d' % round(fps))
+    vid.set(cv2.CAP_PROP_POS_FRAMES, 0) 
 
-    vid.set(cv2.CAP_PROP_POS_FRAMES, 0)             # 현재 프레임 번호를 0으로 설정
-
-    count = 0   # 프레임 카운팅용 변수
-    savecnt = 0 # 저장 카운팅용 변수
-    while(vid.isOpened()):      # 동영상이 정상적으로 불러와졌으면 loop 동작
-        ret, frame = vid.read() # 프레임 읽기
-
-        # print ('%d' % count + '%s' % ret)
-        # assert ret,'False frame %d/' % count + '%d' % frame_num
-
-        # 리턴이 없으면 해당 루프 무시
+    count = 0  
+    savecnt = 0
+    
+    while(vid.isOpened()):
+        ret, frame = vid.read()
+        
         if not ret:
             continue
-
-        # cv2.imshow('video', frame)
-        # if cv2.waitKey(1) & 0xFF == ord('q'):
-        #     break
-
-        # 1초 마다 프레임 영상 저장 (FPS가 15이기 때문에 count와 15 mod 연산)
-        # if np.mod(count,5) == 0:
-        #     cv2.imwrite(osp.join(save_path, 'frame_2107231100_{0:05d}.png'.format(count)), frame)
-        #     print('Process: %d/' % count+ '%d' % frame_num)
-        #     savecnt += 1
 
         if np.mod(count,60) == 0:
         # if count < 995:
             cv2.imwrite(osp.join(save_path, name+'_{0:05d}.jpg'.format(count)), frame)
             print('Process: %d/' % count+ '%d' % frame_num)
             savecnt += 1
-
-        # 현재 프레임 번호가 총 프레임 수보다 크면 루프 탈출
+            
         if vid.get(cv2.CAP_PROP_POS_FRAMES) >= (frame_num):
             break
 
         count += 1
         # print(count)
 
-    vid.release()   # 비디오 해제
+    vid.release()
     # cv2.destroyAllWindows()
 
     print('%d/' % (savecnt) + '%d frames are saved' % frame_num)
