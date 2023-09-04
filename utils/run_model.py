efafa7b8b02c0ccf1ae9ab1e2af968611723166b
dkifty <143667937+dkifty@users.noreply.github.com>
2023-09-04 15:36:35 +0900

    Update run_model.py

--- utils/run_model.py
+++ utils/run_model.py
@@ -17,79 +17,6 @@ for a in labels:
 label_list.sort()
 label_name = label_list
 
-def detectron_configs(model = False, NUM_WORKERS = 2, IMS_PER_BATCH = 2, ITER = 10000):
-    global cfg
-         
-    dataset_name = 'data_dataset_coco_train'
-    
-    if not dataset_name in DatasetCatalog.list():
-        for d in ["data_dataset_coco_train", "data_dataset_coco_test"]:
-            register_coco_instances(f"{d}", {}, f"{d}/annotations.json", f"{d}/images")
-        metadata = MetadataCatalog.get("data_dataset_coco_train")
-
-    cfg = get_cfg()
-    cfg.DATASETS.TRAIN = ("data_dataset_coco_train",) ## 위에서 등록했던 dataset name_train 을 적어주기
-    cfg.DATASETS.TEST = ()
-    cfg.DATALOADER.NUM_WORKERS = NUM_WORKERS
-    cfg.SOLVER.IMS_PER_BATCH = IMS_PER_BATCH
-    cfg.SOLVER.BASE_LR = 0.00025
-    cfg.SOLVER.MAX_ITER = ITER
-    cfg.MODEL.ROI_HEADS.NUM_CLASSES = len(label_name) - 1
-    cfg.MODEL.RETINANET.NUM_CLASSES = len(label_name) - 1
-    
-    if 'fasterrcnn' in model:
-        backbone_1 = model.split('_')[0]
-        backbone_2 = model.split('_')[1]
-        backbone_3 = model.split('_')[2]
-        schedule  = model.split('_')[3]
-        
-        _1 = backbone_1[:-4]
-        _2 = backbone_1[-4:]
-        config_path = 'COCO-Detection/'+_1+'_'+_2+'_'+backbone_2[0]+'_'+backbone_2[1:]+'_'+backbone_3+'_'+schedule+'.yaml'
-        cfg.merge_from_file(model_zoo.get_config_file(config_path))
-        cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url(config_path)
-            
-    elif 'fastrcnn' in model:
-        backbone_1 = model.split('_')[0]
-        backbone_2 = model.split('_')[1]
-        backbone_3 = model.split('_')[2]
-        schedule  = model.split('_')[3]
-        _1 = backbone_1[:-4]
-        _2 = backbone_1[-4:]
-        config_path = 'COCO-Detection/'+_1+'_'+_2+'_'+backbone_2[0]+'_'+backbone_2[1:]+'_'+backbone_3+'_'+schedule+'.yaml'
-        cfg.merge_from_file(model_zoo.get_config_file(config_path))
-        cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url(config_path)
-            
-    elif 'retinanet' in model:
-        backbone_1 = model.split('_')[0]
-        backbone_2 = model.split('_')[1]
-        backbone_3 = model.split('_')[2]
-        schedule  = model.split('_')[3]
-        config_path = 'COCO-Detection/'+backbone_1+'_'+backbone_2[0]+'_'+backbone_2[1:]+'_'+backbone_3+'_'+schedule+'.yaml'
-        cfg.merge_from_file(model_zoo.get_config_file(config_path))
-        cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url(config_path)
-        
-    elif 'rpn' in model:
-        backbone_1 = model.split('_')[0]
-        backbone_2 = model.split('_')[1]
-        backbone_3 = model.split('_')[2]
-        schedule  = model.split('_')[3]
-        config_path = 'COCO-Detection/'+backbone_1+'_'+backbone_2[0]+'_'+backbone_2[1:]+'_'+backbone_3+'_'+schedule+'.yaml'
-        cfg.merge_from_file(model_zoo.get_config_file(config_path))
-        cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url(config_path)
-            
-    elif 'maskrcnn' in model:
-        backbone_1 = model.split('_')[0]
-        backbone_2 = model.split('_')[1]
-        backbone_3 = model.split('_')[2]
-        schedule  = model.split('_')[3]
-        _1 = backbone_1[:-4]
-        _2 = backbone_1[-4:]
-        config_path = 'COCO-InstanceSegmentation/'+_1+'_'+_2+'_'+backbone_2[0]+'_'+backbone_2[1:]+'_'+backbone_3+'_'+schedule+'.yaml'
-        cfg.merge_from_file(model_zoo.get_config_file(config_path))
-        cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url(config_path)
-   
-
 def run_model(model=None, resize_img=1024, batch=16, epochs=200, FOLDERS_COCO = ['./data_dataset_coco_train', './data_dataset_coco_valid', './data_dataset_coco_test'], NUM_WORKERS = 2, IMS_PER_BATCH = 2, ITER = 12000):
     if 'yolov5' in model:
         yolov5_run = 'python3 yolov5/train.py --img ' + str(resize_img) + ' --batch ' + str(batch) + ' --epochs ' + str(epochs) + ' --data yolo_configs/data/custom.yaml --cfg yolo_configs/models/custom_' + model + '.yaml --name custom_results_' + model + '.yaml --cache'
@@ -210,3 +137,7 @@ def run_model(model=None, resize_img=1024, batch=16, epochs=200, FOLDERS_COCO = 
         trainer = DefaultTrainer(cfg) 
         trainer.resume_or_load(resume=False)
         trainer.train()
+        
+    elif 'yolact' in model:
+        yolact_run = 'python yolact/train.py --config='+model+'_custom_config --batch_size='+batch
+        os.system(yolact_run)
