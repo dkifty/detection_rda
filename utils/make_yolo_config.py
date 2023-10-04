5d5ed5b9b1e875a07bb89c548048751c6c104e72
Woo-Joo Choi <143667937+dkifty@users.noreply.github.com>
2023-10-04 11:08:48 +0900

    Update make_yolo_config.py

--- utils/make_yolo_config.py
+++ utils/make_yolo_config.py
@@ -39,20 +39,20 @@ sys.path.append('./yolov5')
 ########################################################################################################################
 
 with open('labels.txt', 'r') as label:
-	labels = label.readlines()
+    labels = label.readlines()
 label_list = []
 for a in labels:
-	label_list.append(a.rstrip())
+    label_list.append(a.rstrip())
 label_list.sort()
 label_name = label_list
 
 label_list_check = label_list
 label_list_check_ = []
 for label_list_check__ in  label_list:
-	if label_list_check__ == '__ignore__' or label_list_check__ == '_background_':
-        	pass
-        else:
-        	label_list_check_.append(label_list_check__)
+    if label_list_check__ == '__ignore__' or label_list_check__ == '_background_':
+        pass
+    else:
+        label_list_check_.append(label_list_check__)
 label_list_check_.sort()
 
 if not os.path.exists('./yolo_configs'):
