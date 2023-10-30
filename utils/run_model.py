83c71cb9575a9b89cee4d99a8bfdfd76c5b8a3a7
Woo-Joo Choi <143667937+dkifty@users.noreply.github.com>
2023-10-30 17:42:44 +0900

    Update run_model.py

--- utils/run_model.py
+++ utils/run_model.py
@@ -209,6 +209,7 @@ def run_model(model=None, train=True, val = True, test = True, iou = 0.5, resize
 	from detectron2.config import get_cfg
 	from detectron2 import model_zoo
 	from detectron2.evaluation import COCOEvaluator, inference_on_dataset
+	from detectron2.utils.visualizer import ColorMode, Visualizer
         
 	dataset_name = 'data_dataset_coco_train'
         
